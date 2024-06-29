35a33f9c661bfd2bb7950fc46f86b19798ed318e
Rizki Miftha <riskimifta@gmail.com>
2024-06-29 08:59:36 -0700

    Pulled energy saving data

---
+++ pages/api/graph/proxy.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    const { zipcode, household_income, household_size } = req.query;
+    const url = 'http://127.0.0.1:5000/get_data';
+    const body = {
+        zipcode,
+        household_income,
+        household_size,
+    };
+    console.log(body);
+
+    try {
+        const response = await axios({
+            url,
+            params: body,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        res.status(response.status).json(response.data);
+    } catch (error) {
+        res.status(error.response ? error.response.status : 500).json(error.response ? error.response.data : { error: 'Internal Server Error' });
+    }
+}
